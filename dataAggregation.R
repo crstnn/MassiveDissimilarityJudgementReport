460e8420b623527ce449895d4b995b5484631221
crstnn <54381541+crstnn@users.noreply.github.com>
2022-01-19 11:20:02 +1100

    basic removal of subject code included. reorganised code.

---
+++ dataAggregation.R
@@ -0,0 +1,75 @@
+library(tidyverse)
+library(ramify)
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(viridis)
+library(MASS)
+library(hrbrthemes)
+library(scales)
+library(broom)
+library(pglm)
+
+# matching regex of any amount of characters then followed by .csv
+files <- list.files(path="./data", pattern="*.csv", full.names=TRUE)
+filesizes <- file.size(files)
+
+truthColourTableColumnNames <- c("r1", "g1", "b1", "r2", "g2", "b2")
+truthColourTable <- read.csv("./colourcodes/colourcodes.csv", header=F)
+names(truthColourTable) <- truthColourTableColumnNames
+
+files <- files[-(which(filesizes < 4))]
+
+pilotdata <- sapply(files, read.csv, simplify=FALSE) %>% bind_rows(.id = "fileId")
+
+# changing realcomparison from zero-indexed to one-indexed
+
+participantsIDFrame <- data.frame(unique(pilotdata$participant))
+
+pilotdata <- pilotdata %>% arrange("participant")
+
+# variables of interest from collected data
+trial_vars<- c( "participant", "practice_comparison", "pracsimilarity", "realcomparison", "similarity", "response_time", "trials_2.thisN") 
+catch_vars<- c("participant", "catch_response_time", "catchnumberprac", "catchpracsimilarity", "catchnumber", "catchsimilarity", "catchRT", "catchtrialorder")
+trialdata <- (pilotdata %>% filter(!is.na(realcomparison)))[trial_vars]
+catchdata <- (pilotdata %>% filter(!is.na(catchnumber)))[catch_vars]
+trialdata = rename(trialdata, dissimilarity = similarity)
+
+trialdata$realcomparison <- trialdata$realcomparison + 1
+
+
+rgb2hex <- function(r, g, b) {rgb(r, g, b, maxColorValue = 255)}
+
+# compartmentalising the two dots presented to participants
+firstColourSet <- truthColourTable[,truthColourTableColumnNames[1:3]]
+secondColourSet <- truthColourTable[,truthColourTableColumnNames[4:6]]
+
+names(firstColourSet) <- names(secondColourSet) <- c("r", "g", "b")
+
+#conversion of 3 RGB columns in both colour tables to HEX for easier manipulation
+firstColourSetHEX <- apply(firstColourSet, 1, function (x) rgb2hex(x[1], x[2], x[3]))
+secondColourSetHEX <- apply(secondColourSet, 1, function (x) rgb2hex(x[1], x[2], x[3]))
+
+
+ggplot(trialdata, aes(x=response_time)) + geom_histogram() + scale_x_continuous(limits=c(0, 5))
+ggplot(catchdata, aes(x=catch_response_time)) + geom_histogram() + scale_x_continuous(limits=c(0, 15))
+ggplot(trialdata, aes(x=dissimilarity)) + geom_histogram(bins = 8) + scale_x_continuous(limits=c(0, 7))
+
+
+ggplot(filter(trialdata, participant == participantsIDFrame[400,]), aes(x=response_time)) + geom_histogram() + scale_x_continuous(limits=c(0, 5))
+
+
+catchdata <- catchdata %>% mutate(is_valid_catch_response = if_else(catchnumber == catchsimilarity, 1, 0), is_invalid_catch_response = if_else(catchnumber != catchsimilarity, 1, 0))
+catchdataParticipant <- catchdata %>% group_by(participant) %>% 
+  summarise(correct_responses=sum(is_valid_catch_response), incorrect_responses = sum(is_invalid_catch_response))
+
+catchdataParticipant$total <- catchdataParticipant$correct_responses + catchdataParticipant$incorrect_responses
+catchdataParticipant$accuracy <- (catchdataParticipant$correct_responses/catchdataParticipant$total) * 100
+
+hist(catchdataParticipant$accuracy)
+
+set.seed(101)
+my.mle<-fitdistr(filter(trialdata, participant == participantsIDFrame[400,])$response_time, densfun="gamma")
+dgamma(0.9, coef(my.mle)[[1]], coef(my.mle)[[2]])
+
+# source("preliminaryGraphsAndAnalysis.R")
--- preliminaryGraphsAndAnalysis.R
+++ preliminaryGraphsAndAnalysis.R
@@ -1,54 +1,3 @@
-library(tidyverse)
-library(ramify)
-library(dplyr)
-library(ggplot2)
-library(reshape2)
-library(viridis)
-library(MASS)
-library(hrbrthemes)
-library(scales)
-library(broom)
-library(pglm)
-
-# matching regex of any amount of characters then followed by .csv
-files <- list.files(path="./data", pattern="*.csv", full.names=TRUE)
-filesizes <- file.size(files)
-
-truthColourTableColumnNames <- c("r1", "g1", "b1", "r2", "g2", "b2")
-truthColourTable <- read.csv("./colourcodes/colourcodes.csv", header=F)
-names(truthColourTable) <- truthColourTableColumnNames
-
-files <- files[-(which(filesizes < 4))]
-
-pilotdata <- sapply(files, read.csv, simplify=FALSE) %>% bind_rows(.id = "fileId")
-
-# changing realcomparison from zero-indexed to one-indexed
-
-participantsIDFrame <- data.frame(unique(pilotdata$participant))
-
-pilotdata <- pilotdata %>% arrange("participant")
-
-# variables of interest from collected data
-trial_vars<- c( "participant", "practice_comparison", "pracsimilarity", "realcomparison", "similarity", "response_time", "trials_2.thisN") 
-catch_vars<- c("participant", "catch_response_time", "catchnumberprac", "catchpracsimilarity", "catchnumber", "catchsimilarity", "catchRT", "catchtrialorder")
-trialdata <- (pilotdata %>% filter(!is.na(realcomparison)))[trial_vars]
-catchdata <- (pilotdata %>% filter(is.na(realcomparison)))[catch_vars]
-
-trialdata$realcomparison <- trialdata$realcomparison + 1
-
-
-rgb2hex <- function(r, g, b) {rgb(r, g, b, maxColorValue = 255)}
-
-# compartmentalising the two dots presented to participants
-firstColourSet <- truthColourTable[,truthColourTableColumnNames[1:3]]
-secondColourSet <- truthColourTable[,truthColourTableColumnNames[4:6]]
-
-names(firstColourSet) <- names(secondColourSet) <- c("r", "g", "b")
-
-#conversion of 3 RGB columns in both colour tables to HEX for easier manipulation
-firstColourSetHEX <- apply(firstColourSet, 1, function (x) rgb2hex(x[1], x[2], x[3]))
-secondColourSetHEX <- apply(secondColourSet, 1, function (x) rgb2hex(x[1], x[2], x[3]))
-
 colourSetHEX <- data.frame(firstColour = firstColourSetHEX, secondColour = secondColourSetHEX)
 
 rowsTotalSetHEX <- rbind(data.frame(colour = firstColourSetHEX), data.frame(colour = secondColourSetHEX)) %>% group_by_all %>% count
@@ -57,6 +6,7 @@ rowsTotalSetHEX <- rbind(data.frame(colour = firstColourSetHEX), data.frame(colo
 countOfUniqueRowsFirstSet <- length(unique(firstColourSetHEX))
 countOfUniqueRowsSecondSet <- length(unique(secondColourSetHEX))
 countOfUniqueRowsTotalSet <- length(rowsTotalSetHEX$colour)
+
 
 # set lower triangle of matrix to NA - this gives heatmap its unique upper triangle
 upperTriangularMatrix <- tril(matrix(1, ncol = countOfUniqueRowsTotalSet, nrow = countOfUniqueRowsTotalSet), diag = FALSE)
@@ -76,26 +26,26 @@ for (i in 1:nrow(trialdata)){
   currentParticipantOBS <- trialdata[i,]
   currParticipantID <- currentParticipantOBS[,"participant"]
   if(i == 1){
-
+    
     previousParticipantID <- currParticipantID
   }
   isNewParticipant <- previousParticipantID != currParticipantID
   if (isNewParticipant){
-
+    
     currentRealComparisionCount <- as.data.frame(matrix(0, nrow(colourSetHEX), 1))
     previousParticipantID <- currParticipantID
   }
   
   currRC <- currentParticipantOBS[,"realcomparison"]
   
-
+  
   currentRealComparisionCount[currRC,] <- currentRealComparisionCount[currRC,] + 1
-
+  
   # counts only the first pass of each participant
   if(currentRealComparisionCount[currRC,] == 1){
-
+    
     HEX1 <- colourSetHEX[currRC,1]
-
+    
     HEX2 <- colourSetHEX[currRC,2]
     
     if(is.na(uniqueColourCountDF[HEX1, HEX2])){
@@ -118,16 +68,16 @@ matrixToLongFormat <- function (uniqueColourCountDF){
     for(c in 1:ncol(uniqueColourCountDF)){
       if (!is.na(uniqueColourCountDF[r, c])){
         longUniqueColourCountDF <- 
-            rbind(longUniqueColourCountDF, c(colnames(uniqueColourCountDF)[r], 
-                                             colnames(uniqueColourCountDF)[c], 
-                                             uniqueColourCountDF[r, c]))
+          rbind(longUniqueColourCountDF, c(colnames(uniqueColourCountDF)[r], 
+                                           colnames(uniqueColourCountDF)[c], 
+                                           uniqueColourCountDF[r, c]))
       }
     }
   }
   colnames(longUniqueColourCountDF) <- c("colour1", "colour2", "count")
   longUniqueColourCountDF$count <- as.numeric(longUniqueColourCountDF$count)
   return(longUniqueColourCountDF)
-
+  
 }
 
 longUniqueColourCountDF <- matrixToLongFormat(uniqueColourCountDF)
@@ -219,7 +169,7 @@ for (i in 1:nrow(trialdata)){
   
   currRC <- currentParticipantOBS[,"realcomparison"]
   
-    
+  
   currentRealComparisionCount[currRC,] <- currentRealComparisionCount[currRC,] + 1
   
   HEX1 <- colourSetHEX[[currRC,1]]
@@ -228,8 +178,8 @@ for (i in 1:nrow(trialdata)){
   
   # counts of only the first pass of each participant and the two HEX values are the same
   if(currentRealComparisionCount[currRC,] == 1 & HEX1 == HEX2){
-
-    sameColourDissimilarity[HEX1, "sum"] <- sameColourDissimilarity[HEX1, "sum"] + currentParticipantOBS[, "similarity"]
+    
+    sameColourDissimilarity[HEX1, "sum"] <- sameColourDissimilarity[HEX1, "sum"] + currentParticipantOBS[, "dissimilarity"]
     
     sameColourDissimilarity[HEX1, "count"] <- sameColourDissimilarity[HEX1, "count"] + 1
   }
@@ -240,7 +190,7 @@ sameColourDissimilarity$average <- sameColourDissimilarity$sum / sameColourDissi
 sameColourDissimilarity[is.na(sameColourDissimilarity)] = 0 # for division by zero case
 
 
-ggplot(sameColourDissimilarity, aes(x=average)) + geom_histogram(bins=31) + scale_x_continuous(limits=c(0, 2.3))
+ggplot(sameColourDissimilarity, aes(x=average)) + geom_histogram(bins=31)
 
 ggplot(sameColourDissimilarity, aes(x=count)) + geom_histogram(bins=30) + scale_x_continuous(limits=c(0, 31))
 
@@ -250,15 +200,15 @@ aggDataDF$rDiff <- abs(aggDataDF$r1 - aggDataDF$r2)
 aggDataDF$gDiff <- abs(aggDataDF$g1 - aggDataDF$g2)
 aggDataDF$bDiff <- abs(aggDataDF$b1 - aggDataDF$b2)
 aggDataDF$RGBdiffSum <- aggDataDF$rDiff + aggDataDF$gDiff + aggDataDF$bDiff
-aggDataDF$similarity <- as.factor(aggDataDF$similarity)
+aggDataDF$dissimilarity <- as.factor(aggDataDF$dissimilarity)
 
-ggplot(aggDataDF, aes(x=RGBdiffSum, y=similarity)) + 
+ggplot(aggDataDF, aes(x=RGBdiffSum, y=dissimilarity)) + 
   geom_violin()
 
 # hist(aggDataDF$RGBdiffSum)
 
 # taking the mean of each participant (if double pass was reached)
-aggDataDFStabilised <- trialdata %>% group_by(participant, realcomparison) %>% summarise(dissimilarity_mean=(mean(similarity)), response_time_mean = mean(response_time))
+aggDataDFStabilised <- trialdata %>% group_by(participant, realcomparison) %>% summarise(dissimilarity_mean=(mean(dissimilarity)), response_time_mean = mean(response_time))
 
 aggDataDFStabilised$dissimilarity_mean <- as.factor(aggDataDFStabilised$dissimilarity_mean)
 
@@ -284,10 +234,10 @@ getConfInterval <- function(m, level=0.99){
 }
 
 
-logitModel <- polr(formula = similarity ~ rDiff + gDiff + bDiff + response_time, data = aggDataDF, method = "logistic")
+logitModel <- polr(formula = dissimilarity ~ rDiff + gDiff + bDiff + response_time, data = aggDataDF, method = "logistic")
 
 
-probitModel <- polr(formula = similarity ~ rDiff + gDiff + bDiff + response_time, data = aggDataDF, method = "probit")
+probitModel <- polr(formula = dissimilarity ~ rDiff + gDiff + bDiff + response_time, data = aggDataDF, method = "probit")
 
 options(scipen=999)
 summary(logitModel)
@@ -299,12 +249,12 @@ getConfInterval(probitModel)
 
 
 
-logitFEModel <- pglm(similarity ~ rDiff + gDiff + bDiff + response_time, data = aggDataDF,
+logitFEModel <- pglm(dissimilarity ~ rDiff + gDiff + bDiff + response_time, data = aggDataDF,
                      index = c('participant'), family = ordinal('logit'), model = 'random', na.action = na.omit, chunksize = 5000)
 
 summary(logitFEModel)
 
-probitFEModel <- pglm(similarity ~ rDiff + gDiff + bDiff + response_time, data = aggDataDF, index = 'participant', family = ordinal('probit'), model = 'random', method = 'bfgs', start = NULL)
+probitFEModel <- pglm(dissimilarity ~ rDiff + gDiff + bDiff + response_time, data = aggDataDF, index = 'participant', family = ordinal('probit'), model = 'random', method = 'bfgs', start = NULL)
 
 
 
